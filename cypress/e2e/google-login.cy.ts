9ce8c152b605b9e7b017dab1d398d98332e4a4c5
D-Antonelli <derya.antonelli@gmail.com>
2024-08-02 17:37:14 +0100

    fixing type errors

--- cypress/e2e/google-login.cy.ts
+++ cypress/e2e/google-login.cy.ts
@@ -1,13 +1,17 @@
+// default set-up from next-auth documentation. needs some fixes
+
 describe("Login page", () => {
   before(() => {
-    cy.log(`Visiting login page`)
-    cy.visit("http://localhost:3000")
-  })
+    cy.log(`Visiting login page`);
+    cy.visit("http://localhost:3000");
+  });
+
   it("Login with Google", () => {
-    const username = Cypress.env("GOOGLE_USER")
-    const password = Cypress.env("GOOGLE_PW")
-    const loginUrl = Cypress.env("SITE_NAME")
-    const cookieName = Cypress.env("COOKIE_NAME")
+    const username: string = Cypress.env("GOOGLE_USER");
+    const password: string = Cypress.env("GOOGLE_PW");
+    const loginUrl: string = Cypress.env("SITE_NAME");
+    const cookieName: string = Cypress.env("COOKIE_NAME");
+
     const socialLoginOptions = {
       username,
       password,
@@ -15,38 +19,30 @@ describe("Login page", () => {
       headless: true,
       logs: true,
       isPopup: true,
-      loginSelector: `a[href="${Cypress.env(
-        "SITE_NAME"
-      )}/api/auth/signin/google"]`,
-      postLoginSelector: ".unread-count",
-    }
+      loginSelector: `a[href="${Cypress.env("SITE_NAME")}/api/auth/signin/google"]`,
+      postLoginSelector: ".test-stripe",
+    };
 
-    return cy
-      .task("GoogleSocialLogin", socialLoginOptions)
-      .then(({ cookies }) => {
-        cy.clearCookies()
+    return cy.task("GoogleSocialLogin", socialLoginOptions).then((result: any) => {
+      const {cookies} = result;
 
-        const cookie = cookies
-          .filter((cookie) => cookie.name === cookieName)
-          .pop()
-        if (cookie) {
-          cy.setCookie(cookie.name, cookie.value, {
-            domain: cookie.domain,
-            expiry: cookie.expires,
-            httpOnly: cookie.httpOnly,
-            path: cookie.path,
-            secure: cookie.secure,
-          })
+      cy.clearCookies();
 
-          Cypress.Cookies.defaults({
-            preserve: cookieName,
-          })
+      const cookie = cookies.filter((cookie: any) => cookie.name === cookieName).pop();
+      if (cookie) {
+        cy.setCookie(cookie.name, cookie.value, {
+          domain: cookie.domain,
+          expiry: cookie.expires,
+          httpOnly: cookie.httpOnly,
+          path: cookie.path,
+          secure: cookie.secure,
+        });
 
-          // remove the two lines below if you need to stay logged in
-          // for your remaining tests
-          cy.visit("/api/auth/signout")
-          cy.get("form").submit()
-        }
-      })
-  })
-})
+        // remove the two lines below if you need to stay logged in
+        // for your remaining tests
+        cy.visit("/api/auth/signout");
+        cy.get("form").submit();
+      }
+    });
+  });
+});
